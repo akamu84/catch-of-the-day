5f32eef9229ae9b25aea44d71acf279e2d6ee7bf
Adam Lemelin <akamu84@gmail.com>
2019-10-20 09:09:55 -0500

    Added store picker component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        )
+    }
+}
+
+export default StorePicker;
