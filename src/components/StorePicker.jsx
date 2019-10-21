93065d4932039cd143a21ed5c31ecdd3b3b90205
Adam Lemelin <akamu84@gmail.com>
2019-10-20 22:13:43 -0500

    Added routing from store picker to store

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { getFunName } from "../helpers";
 
-const StorePicker = () => {
-  const goToStore = event => {
+class StorePicker extends React.Component {
+  storeNameRef = React.createRef();
+
+  goToStore = event => {
     event.preventDefault();
-    console.log("Going to store");
+    const storeName = this.storeNameRef.current.value;
+    this.props.history.push(`/store/${storeName}`);
   };
 
-  return (
-    <form className="store-selector" onSubmit={goToStore}>
-      <h2>Please Enter A Store</h2>
-      <input
-        type="text"
-        required
-        placeholder="Store Name"
-        defaultValue={getFunName()}
-      />
-      <button type="submit">Visit Store →</button>
-    </form>
-  );
-};
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          ref={this.storeNameRef}
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
