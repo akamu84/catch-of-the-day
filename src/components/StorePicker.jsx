f7f29a75bfd39e5c4ce6b58142667ed70ddf1a69
Adam Lemelin <akamu84@gmail.com>
2019-10-20 21:53:35 -0500

    Added onSubmit handler for store picker

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { getFunName } from "../helpers";
 
-const StorePicker = () => (
-  <form className="store-selector">
-    <h2>Please Enter A Store</h2>
-    <input
-      type="text"
-      required
-      placeholder="Store Name"
-      defaultValue={getFunName()}
-    />
-    <button type="submit">Visit Store →</button>
-  </form>
-);
+const StorePicker = () => {
+  const goToStore = event => {
+    event.preventDefault();
+    console.log("Going to store");
+  };
+
+  return (
+    <form className="store-selector" onSubmit={goToStore}>
+      <h2>Please Enter A Store</h2>
+      <input
+        type="text"
+        required
+        placeholder="Store Name"
+        defaultValue={getFunName()}
+      />
+      <button type="submit">Visit Store →</button>
+    </form>
+  );
+};
 
 export default StorePicker;
