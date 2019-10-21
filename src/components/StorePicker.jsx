391a6c829054b26efe98d608106e137b00852d83
Adam Lemelin <akamu84@gmail.com>
2019-10-20 20:54:56 -0500

    Converted components to stateless functional

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -1,15 +1,11 @@
-import React from 'react';
+import React from "react";
 
-class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button type="submit">Visit Store</button>
-      </form>
-    );
-  }
-}
+const StorePicker = () => (
+  <form className="store-selector">
+    <h2>Please Enter A Store</h2>
+    <input type="text" required placeholder="Store Name" />
+    <button type="submit">Visit Store</button>
+  </form>
+);
 
 export default StorePicker;
