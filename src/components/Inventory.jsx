5d91d49cdbbd9216f4e4c6a7d3d05931ec98e2dc
Adam Lemelin <akamu84@gmail.com>
2019-10-28 19:37:56 -0500

    Populating fish state with sample objects

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -4,6 +4,7 @@ import AddFishForm from "./AddFishForm";
 const Inventory = props => (
   <div className="inventory">
     <AddFishForm addFish={props.addFish} />
+    <button onClick={props.loadSampleFishes}>Load Sample Fishes</button>
   </div>
 );
 
